e6da2f3850466c236c307622b2a1324971f6ab5f
Ignacio Vera <ivera@apache.org>
2020-01-29 14:10:19 +0100

    back to basic

--- lucene/core/src/java/org/apache/lucene/util/bkd/DocIdsWriter.java
+++ lucene/core/src/java/org/apache/lucene/util/bkd/DocIdsWriter.java
@@ -64,10 +64,10 @@ class DocIdsWriter {
         max |= Integer.toUnsignedLong(docIds[start + i]);
       }
       if (max <= 0xffffff) {
-        if (runLenDocs < count / 2.5) {
-          // runLen24 is too slow for decoding
-          // writeRunLen24(docIds, start, count, out);
-          writeRunLen(docIds, start, count, out);
+        if (runLenDocs < count / 2) {
+          // runLen24 is too slow for decoding, so instead
+          // if dividing by 1.34, we do it by two
+          writeRunLen24(docIds, start, count, out);
         } else {
           writeInt24(docIds, start, count, out);
         }
