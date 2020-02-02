ae94c3fa10ac83474939d1a1a2b82eb9c3dceb6d
Ignacio Vera <ivera@apache.org>
2020-02-02 12:00:26 +0100

    experiments

--- lucene/core/src/test/org/apache/lucene/util/bkd/TestDocIdsWriter.java
+++ lucene/core/src/test/org/apache/lucene/util/bkd/TestDocIdsWriter.java
@@ -21,8 +21,6 @@ import java.util.Arrays;
 
 import org.apache.lucene.index.PointValues.IntersectVisitor;
 import org.apache.lucene.index.PointValues.Relation;
-import org.apache.lucene.store.ByteArrayDataInput;
-import org.apache.lucene.store.ByteBuffersDataOutput;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.IOContext;
 import org.apache.lucene.store.IndexInput;
