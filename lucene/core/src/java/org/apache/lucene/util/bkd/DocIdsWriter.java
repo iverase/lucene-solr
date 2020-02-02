9b0c9e143a8223cec6fe6697626560d6101473ab
Ignacio Vera <ivera@apache.org>
2020-02-02 15:33:47 +0100

    experiments

--- lucene/core/src/java/org/apache/lucene/util/bkd/DocIdsWriter.java
+++ lucene/core/src/java/org/apache/lucene/util/bkd/DocIdsWriter.java
@@ -380,12 +380,46 @@ class DocIdsWriter {
     int i;
     int index = 0;
     int runLen = 0;
-    for (i = 0; i < count - 1; i += 2) {
+    for (i = 0; i < count - 7; i += 8) {
       long l = in.readLong();
       runLen += (int) (l >>> 56);
       int doc = (int) (l >>> 32) & 0xffffff;
       Arrays.fill(docIDs, index, runLen, doc);
       index = runLen;
+
+      runLen += (int) (l >>> 24) & 0xff;
+      doc = (int) l & 0xffffff;
+      Arrays.fill(docIDs, index, runLen, doc);
+      index = runLen;
+
+      l = in.readLong();
+      runLen += (int) (l >>> 56);
+      doc = (int) (l >>> 32) & 0xffffff;
+      Arrays.fill(docIDs, index, runLen, doc);
+      index = runLen;
+
+      runLen += (int) (l >>> 24) & 0xff;
+      doc = (int) l & 0xffffff;
+      Arrays.fill(docIDs, index, runLen, doc);
+      index = runLen;
+
+      l = in.readLong();
+      runLen += (int) (l >>> 56);
+      doc = (int) (l >>> 32) & 0xffffff;
+      Arrays.fill(docIDs, index, runLen, doc);
+      index = runLen;
+
+      runLen += (int) (l >>> 24) & 0xff;
+      doc = (int) l & 0xffffff;
+      Arrays.fill(docIDs, index, runLen, doc);
+      index = runLen;
+
+      l = in.readLong();
+      runLen += (int) (l >>> 56);
+       doc = (int) (l >>> 32) & 0xffffff;
+      Arrays.fill(docIDs, index, runLen, doc);
+      index = runLen;
+
       runLen += (int) (l >>> 24) & 0xff;
       doc = (int) l & 0xffffff;
       Arrays.fill(docIDs, index, runLen, doc);
