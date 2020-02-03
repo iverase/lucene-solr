08c5e60bcefea2e3726ac6f4cc6fefd604e83f24
Ignacio Vera <ivera@apache.org>
2020-02-03 15:56:26 +0100

    simpler approach

--- lucene/core/src/java/org/apache/lucene/util/bkd/DocIdsWriter.java
+++ lucene/core/src/java/org/apache/lucene/util/bkd/DocIdsWriter.java
@@ -17,6 +17,7 @@
 package org.apache.lucene.util.bkd;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.apache.lucene.index.PointValues.IntersectVisitor;
 import org.apache.lucene.store.DataOutput;
@@ -24,58 +25,107 @@ import org.apache.lucene.store.IndexInput;
 
 class DocIdsWriter {
 
+  private static final byte SORTED = (byte) 0;
+  private static final byte INT24 = (byte) 24;
+  private static final byte INT32 = (byte) 32;
+  private static final byte RUNLEN32 = (byte) 33;
+
+
   private DocIdsWriter() {}
 
   static void writeDocIds(int[] docIds, int start, int count, DataOutput out) throws IOException {
     // docs can be sorted either when all docs in a block have the same value
     // or when a segment is sorted
+    if (count == 0) {
+      out.writeVInt(0);
+      out.writeByte(SORTED);
+      return;
+    }
+    // docs can be sorted either when all docs in a block have the same value
+    // or when a segment is sorted
     boolean sorted = true;
+    int docId = docIds[start];
+    long max = Integer.toUnsignedLong(docId);
+    int runLenDocs = 1;
+    int prevIndex = 0;
     for (int i = 1; i < count; ++i) {
-      if (docIds[start + i - 1] > docIds[start + i]) {
+      if (sorted && docId > docIds[start + i]) {
         sorted = false;
-        break;
+      }
+      if (docId != docIds[start + i]) {
+        docId = docIds[start + i];
+        max |= Integer.toUnsignedLong(docId);
+        runLenDocs++;
+        prevIndex = i;
       }
     }
     if (sorted) {
-      out.writeByte((byte) 0);
-      int previous = 0;
-      for (int i = 0; i < count; ++i) {
-        int doc = docIds[start + i];
-        out.writeVInt(doc - previous);
-        previous = doc;
-      }
+      writeDeltaVInts(docIds, start, count, out);
     } else {
-      long max = 0;
-      for (int i = 0; i < count; ++i) {
-        max |= Integer.toUnsignedLong(docIds[start + i]);
-      }
-      if (max <= 0xffffff) {
-        out.writeByte((byte) 24);
-        for (int i = 0; i < count; ++i) {
-          out.writeShort((short) (docIds[start + i] >>> 8));
-          out.writeByte((byte) docIds[start + i]);
-        }
+      if (runLenDocs < count / 2) {
+        writeRunLen32(docIds, start, count, out);
+      } else if (max <= 0xffffff) {
+        writeInts24(docIds, start, count, out);
       } else {
-        out.writeByte((byte) 32);
-        for (int i = 0; i < count; ++i) {
-          out.writeInt(docIds[start + i]);
-        }
+        writeInts32(docIds, start, count, out);
       }
     }
+  }
+
+  private static void writeDeltaVInts(int[] docIds, int start, int count, DataOutput out) throws IOException {
+    out.writeByte(SORTED);
+    int previous = 0;
+    for (int i = 0; i < count; ++i) {
+      int doc = docIds[start + i];
+      out.writeVInt(doc - previous);
+      previous = doc;
+    }
+  }
+
+  private static void writeInts32(int[] docIds, int start, int count, DataOutput out) throws IOException {
+    out.writeByte(INT32);
+    for (int i = 0; i < count; ++i) {
+      out.writeInt(docIds[start + i]);
+    }
+  }
+
+  private static void writeInts24(int[] docIds, int start, int count, DataOutput out) throws IOException {
+    out.writeByte(INT24);
+    for (int i = 0; i < count; ++i) {
+      out.writeShort((short) (docIds[start + i] >>> 8));
+      out.writeByte((byte) docIds[start + i]);
+    }
+  }
+
+  private static void writeRunLen32(int[] docIds, int start, int count, DataOutput out) throws IOException {
+    out.writeByte(RUNLEN32);
+    int doc = docIds[start];
+    for (int i = 1; i < count; ++i) {
+      if (docIds[start + i] != doc) {
+        out.writeVInt(i);
+        out.writeInt(doc);
+        doc = docIds[start + i];
+      }
+    }
+    out.writeVInt(count);
+    out.writeInt(doc);
   }
 
   /** Read {@code count} integers into {@code docIDs}. */
   static void readInts(IndexInput in, int count, int[] docIDs) throws IOException {
     final int bpv = in.readByte();
     switch (bpv) {
-      case 0:
+      case SORTED:
         readDeltaVInts(in, count, docIDs);
         break;
-      case 32:
+      case INT32:
         readInts32(in, count, docIDs);
         break;
-      case 24:
+      case INT24:
         readInts24(in, count, docIDs);
+        break;
+      case RUNLEN32:
+        readRunLen32(in, count, docIDs);
         break;
       default:
         throw new IOException("Unsupported number of bits per value: " + bpv);
@@ -116,18 +166,28 @@ class DocIdsWriter {
     }
   }
 
+  private static void readRunLen32(IndexInput in, int count, int[] docIDs) throws IOException {
+    int i;
+    for (i = 0; i < count;) {
+      Arrays.fill(docIDs, i, i = in.readVInt(), in.readInt());
+    }
+  }
+
   /** Read {@code count} integers and feed the result directly to {@link IntersectVisitor#visit(int)}. */
   static void readInts(IndexInput in, int count, IntersectVisitor visitor) throws IOException {
-    final int bpv = in.readByte();
+    final byte bpv = in.readByte();
     switch (bpv) {
-      case 0:
+      case SORTED:
         readDeltaVInts(in, count, visitor);
         break;
-      case 32:
+      case INT32:
         readInts32(in, count, visitor);
         break;
-      case 24:
+      case INT24:
         readInts24(in, count, visitor);
+        break;
+      case RUNLEN32:
+        readRunLen32(in, count, visitor);
         break;
       default:
         throw new IOException("Unsupported number of bits per value: " + bpv);
@@ -167,4 +227,18 @@ class DocIdsWriter {
       visitor.visit((Short.toUnsignedInt(in.readShort()) << 8) | Byte.toUnsignedInt(in.readByte()));
     }
   }
+
+  private static void readRunLen32(IndexInput in, int count, IntersectVisitor visitor) throws IOException {
+    int i;
+    for (i = 0; i < count;) {
+      visit(visitor, i, i = in.readVInt(), in.readInt());
+    }
+  }
+
+  private static void visit(IntersectVisitor visitor, int from, int to, int doc) throws IOException {
+    for (int i = from; i < to; i++) {
+      visitor.visit(doc);
+    }
+  }
+
 }
