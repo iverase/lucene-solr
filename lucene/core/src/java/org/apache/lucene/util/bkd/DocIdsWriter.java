cddf9e377f00ad7d992b39d8901d35f6676003e7
Ignacio Vera <ivera@apache.org>
2020-02-06 07:50:08 +0100

    simpler approach

--- lucene/core/src/java/org/apache/lucene/util/bkd/DocIdsWriter.java
+++ lucene/core/src/java/org/apache/lucene/util/bkd/DocIdsWriter.java
@@ -36,7 +36,7 @@ class DocIdsWriter {
   private static final byte INT32 = (byte) 32;
   private static final byte RUNLEN32 = (byte) 33;
 
-  private static final int RUNLEN = 6;
+  private static final int RUNLEN = 8;
 
   private DocIdsWriter() {}
 
